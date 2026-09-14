fb04b71094443d0dac4c2b5752b823fb22d4c14a
ElissonDeSous <elissondsousa@gmail.com>
2026-09-14 10:45:18 -0300

    estilizando a lista de postagens

--- app/pages/lista-postagens/page.jsx
+++ app/pages/lista-postagens/page.jsx
@@ -2,6 +2,8 @@
 import Header from "@/app/componentes/header/header"
 import Image from "next/image"
 import { useEffect, useState } from "react"
+import { MdDelete } from "react-icons/md";
+import { GrView } from "react-icons/gr";
 export default function ListaPostagens() {
    const [postagens, setPostagens] = useState([])
 
@@ -65,36 +67,35 @@ export default function ListaPostagens() {
                      <p>Gerencie suas postagens aqui, exclua, visualize</p>
                   </div>
 
-                  <div className="bg-white rounded-2xl  border-1-left w-full mt-10  flex flex-col items-center  ">
+                  <div className="bg-white rounded-2xl   w-[1100px] mt-10 p-5 ml-20  flex flex-col items-center  ">
                      <div>
-                        <h2 className="text-2xl text-gray-700 mt-5 font-bold">Historico de Postagens</h2>
+                        <h2 className="text-2xl text-gray-700 mt-2 mb-2 font-bold">Historico de Postagens</h2>
                      </div>
 
                      <div className="w-full ">
                         <div className="bg-gray-200 font-bold   h-[50px] border-gray-300 rounded-2xl flex justify-around items-center  w-full">
-                           <h1 className=" flex justify-center items-center h-full">Imagem</h1>
-                           <h1 className=" ] flex  justify-center items-center h-full">Visualizar</h1>
-                           <h1 className="  justify-center flex items-center h-full">Excluir</h1>
+                           <h2 className=" flex justify-center items-center h-full">Imagem</h2>
+                           <h2 className=" ] flex  justify-center items-center h-full">Visualizar</h2>
+                           <h2 className="  justify-center flex items-center h-full">Excluir</h2>
                         </div>
                         {postagens.map((postagens) => {
                            const imagemConvertida = JSON.parse(postagens.conteudo)
                            const conteudo = reviwe(imagemConvertida)
                            console.log(conteudo.imagem)
                            return (
-                           <div key={postagens.Id} className="border-gray-400 border-1 font-bold   rounded-[10px] flex justify-around items-center mb-10  w-full">
-                            <div className="flex flex-col justify-center items-center h-full">
+                           <div key={postagens.Id} className="border-gray-200 border-1 font-bold   rounded-[10px] flex justify-around items-center mb-10 mt-10 w-full">
+                            <div className="flex flex-col justify-center items-center h-[130px]">
                                 {
                                  
                                   conteudo.imagem && 
-                                   <Image src={conteudo.imagem} className=" m-2 " width={80} height={80} alt="imagem do banco" />
+                                   <Image src={conteudo.imagem} className=" m-5 " width={100} height={100} alt="imagem do banco" />
                                      
                                   
                                 }
                                 </div>
-                                 <h1 
+                                 <h2 className=" cursor-pointer hover:text-blue-600  flex  justify-center items-center h-full"><GrView size={40} /></h2>
 
-                                  className="   flex  justify-center items-center h-full">Visualizar</h1>
-                                 <h1 onClick={()=>Delete(postagens.Id)} className=" cursor-pointer  justify-center flex items-center h-full">Excluir</h1>
+                                 <h2 onClick={()=>Delete(postagens.Id)} className=" hover:text-red-500 cursor-pointer  justify-center flex items-center h-full"><MdDelete size={40} /></h2>
                               </div>
                               
                            )
